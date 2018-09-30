97293cd95610d2255d7c1df8bd33dab3801a9bc1
janeduncan <janeduncan@live.co.uk>
2018-09-30 22:12:48 +0100

    Adds Hangar/HangarTest classes

---
+++ src/main/java/Hangar.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Hangar {
+
+    private String name;
+    private Airline airline;
+    private ArrayList<Plane> planes;
+
+    public Hangar(String name, Airline airline) {
+        this.name = name;
+        this.airline = airline;
+        planes = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Airline getAirline() {
+        return this.airline;
+    }
+
+    public int planeCount() {
+        return this.planes.size();
+    }
+
+    public void addPlane(Plane plane) {
+        this.planes.add(plane);
+    }
+
+    public void removePlane(Plane plane) {
+        this.planes.remove(plane);
+    }
+
+}
---
+++ src/test/java/HangarTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HangarTest {
+
+    Hangar hangar;
+    Plane plane;
+
+    @Before
+    public void before() {
+        hangar = new Hangar("A3", Airline.KLM);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("A3", hangar.getName());
+    }
+
+    @Test
+    public void hasAirline() {
+        assertEquals(Airline.KLM, hangar.getAirline());
+    }
+
+    @Test
+    public void startsEmpty() {
+        assertEquals(0, hangar.planeCount());
+    }
+
+    @Test
+    public void canAddPlane() {
+        hangar.addPlane(plane);
+        assertEquals(1, hangar.planeCount());
+    }
+
+}
